136a3ef42d6b583a01c4f9b95f6095fbba511d78
Nabcellent <nabcellent.dev@gmail.com>
2021-12-15 18:14:02 +0300

    Fix Notification validation
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/resources/notification/notification.validation.ts
+++ src/resources/notification/notification.validation.ts
@@ -3,11 +3,15 @@ import Joi from 'joi'
 const create = Joi.object({
     channel: Joi.string().valid('slack', 'sms', 'mail', 'app').required(),
     content: Joi.string().required(),
-    destination: Joi.when('channel', {
-        is: 'sms',
-        then: Joi.alternatives().try(Joi.array().items(Joi.number()), Joi.number()),
-        otherwise: Joi.alternatives().try(Joi.array().items(Joi.string()), Joi.string())
-    }).required(),
+    destination: Joi
+        .when('channel', {
+            is: 'sms',
+            then: Joi.alternatives().try(Joi.array().items(Joi.number()), Joi.number()),
+        }).when('channel', {
+            is: 'mail',
+            then: Joi.alternatives().try(Joi.array().items(Joi.string().email()), Joi.string().email())
+        })
+        .required(),
 })
 
 export default {create}
