2ed90f91eae698109603d4c56bb2b3126f25f2b3
nabcellent <nabcellent.dev@gmail.com>
2022-01-19 21:10:10 +0300

    DB Refactor - added polymorphic relationship to notifications
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/resources/notification/notification.service.ts
+++ src/resources/notification/notification.service.ts
@@ -6,31 +6,33 @@ class NotificationService {
     // Fetch all notifications
     async fetchAll() {
         try {
-            return await Notification.find({}).sort('-_id').populate('notifiable_id')
+            return await Notification.find({})
+                .select(['id', 'destination', 'channel', 'event_type', 'content', 'provider', 'status', 'notifiable_type'])
+                .sort('-_id').populate('notifiable_id', ['data']);
         } catch (err) {
-            log.error(err)
-            throw new Error('Unable to fetch notifications')
+            log.error(err);
+            throw new Error('Unable to fetch notifications');
         }
     }
 
     // Create new notification
     async create(channel: string, destination: string, content: string, event_type: string): Promise<INotification> {
         try {
-            return await Notification.create({channel, destination, content, event_type});
+            return await Notification.create({ channel, destination, content, event_type });
         } catch (e) {
-            log.error(e)
-            throw new Error('Unable to send notification')
+            log.error(e);
+            throw new Error('Unable to send notification');
         }
     }
 
     //  Update notification
     async update(notification: INotification) {
         try {
-            return Notification.updateOne({_id: notification._id}, {$set: notification});
+            return Notification.updateOne({ _id: notification._id }, { $set: notification });
         } catch (e) {
-            throw new Error('Unable to send notification')
+            throw new Error('Unable to send notification');
         }
     }
 }
 
-export default NotificationService
+export default NotificationService;
