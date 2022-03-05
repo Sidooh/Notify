87af5ac54b6637d113102d4346080ae3f77dcc91
nabcellent <nabcellent.dev@gmail.com>
2022-03-05 11:05:45 +0300

    Add Tests
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/http/controllers/__test__/setting/tweak.test.ts
+++ src/http/controllers/__test__/setting/tweak.test.ts
@@ -1,14 +1,15 @@
-import App from '../../../src/app';
 import supertest from 'supertest';
+import App from '../../../../app';
 
 const { app } = new App(Number(process.env.PORT || 4000));
 const request = supertest(app);
 
-it('should return a 201 on successful slack notification creation', async function() {
+it('should return a 201 on successful mail notification creation', async function() {
     await request
         .post('/api/notifications')
         .send({
-            channel: 'slack',
+            channel: 'mail',
+            destination: ['nabcellent.dev@gmail.com'],
             content: 'Hello!'
         })
         .expect(201);
