9dc3065c32dfb9dfa3c11dd9df9b6e2ce43f4616
nabcellent <nabcellent.dev@gmail.com>
2022-03-17 14:55:21 +0300

    Fix Tests
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,18 +2,20 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { Sms } from '../src/channels/sms/WebSMS/Lib/sms';
 import { WebSms } from '../src/channels/sms/WebSMS/Lib/client';
+import axios from 'axios';
 
 let mongo: any;
 
+jest.mock("axios");
 jest.spyOn(new Sms(new WebSms({
     accessKey: String(process.env.WEBSMS_DEV_ACCESS_KEY),
     apiKey   : String(process.env.WEBSMS_DEV_API_KEY),
     clientId : String(process.env.WEBSMS_DEV_CLIENT_ID),
     senderId : String(process.env.WEBSMS_DEV_SENDER_ID)
-})), 'send').mockImplementation(() => {
-    // return new Promise<any>(() => jest.fn())
-    // return jest.fn(() => Promise.resolve({ data: {} }));
-    return Promise.resolve({ data: {} });
+})), 'send').mockImplementation(async () => {
+    const { data } = await axios.post("/SendBulkSMS", {});
+
+    return data;
 });
 
 beforeAll(async () => {
