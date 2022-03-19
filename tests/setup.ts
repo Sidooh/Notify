815d3ef220d849dac3373605c2e045feeffeffc5
nabcellent <nabcellent.dev@gmail.com>
2022-03-19 18:54:40 +0300

    Fix tests & add logging to slack
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,30 +1,7 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { Sms } from '../src/channels/sms/WebSMS/Lib/sms';
-import { WebSms } from '../src/channels/sms/WebSMS/Lib/client';
-import axios from 'axios';
 
 let mongo: any;
-
-/**
- * _________________________________________________________________    MOCKS
- * */
-jest.mock('axios');
-jest.mock('winston-slack-webhook-transport');
-jest.spyOn(new Sms(new WebSms({
-    accessKey: String(process.env.WEBSMS_DEV_ACCESS_KEY),
-    apiKey   : String(process.env.WEBSMS_DEV_API_KEY),
-    clientId : String(process.env.WEBSMS_DEV_CLIENT_ID),
-    senderId : String(process.env.WEBSMS_DEV_SENDER_ID)
-})), 'send').mockImplementation(async () => {
-    const mockedAxios = axios as jest.Mocked<typeof axios>;
-    mockedAxios.post.mockResolvedValueOnce([]);
-
-    const { data } = await mockedAxios.post('/SendBulkSMS', {});
-
-    return data;
-});
-// initMocks()
 
 beforeAll(async () => {
     mongo = await MongoMemoryServer.create();
