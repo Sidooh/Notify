31f17511814361830be493ae16bf59b59f966c8b
Nabcellent <nabcellent.dev@gmail.com>
2022-08-22 13:35:34 +0300

    Adds Sentry
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/utils/validate.env.ts
+++ src/utils/validate.env.ts
@@ -1,4 +1,4 @@
-import { bool, cleanEnv, num, port, str } from 'envalid';
+import { bool, cleanEnv, num, port, str, url } from 'envalid';
 
 export default function validateEnv() {
     return cleanEnv(process.env, {
@@ -25,11 +25,11 @@ export default function validateEnv() {
         // MAIL_USERNAME: str(),
         // MAIL_PASSWORD: str(),
 
-        AT_SMS_API_KEY : str(),
-        AT_SMS_USERNAME: str(),
+        AT_SMS_API_KEY     : str(),
+        AT_SMS_USERNAME    : str(),
         AT_SMS_DEV_API_KEY : str(),
         AT_SMS_DEV_USERNAME: str(),
-        AT_SMS_FROM    : str(),
+        AT_SMS_FROM        : str(),
 
         WEBSMS_SANDBOX: bool({ default: true }),
         // WEBSMS_API_URL   : url(),
@@ -47,7 +47,10 @@ export default function validateEnv() {
         SLACK_LOGGING: str({
             default: 'enabled',
             choices: ['enabled', 'disabled']
-        })
+        }),
+
+        SENTRY_DSN               : url({ default: null }),
+        SENTRY_TRACES_SAMPLE_RATE: num({ default: 0.0 })
     });
 }
 
