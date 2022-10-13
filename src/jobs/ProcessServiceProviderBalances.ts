d3637cd46dfc538859172c08cecdff6849ae984e
Nabcellent <nabcellent.dev@gmail.com>
2022-10-13 03:42:28 +0300

    CU-3j8gkyv - Add SP Balance Check Job
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/jobs/ProcessServiceProviderBalances.ts
+++ src/jobs/ProcessServiceProviderBalances.ts
@@ -22,18 +22,18 @@ export const ProcessServiceProviderBalances = () => {
                 earnings: productBalances.tanda[0].balances[1].available
             };
         const AT = {
-            sms    : await new ATService(smsSettings.africastalking_env).balance(),
-            airtime: Number(productBalances.at.data.UserData.balance.slice(3)),
+            // sms    : await new ATService(smsSettings.africastalking_env).balance(),
+            // airtime: Number(productBalances.at.data.UserData.balance.slice(3)),
             ussd   : await new ATService(smsSettings.africastalking_env, ATApp.USSD).balance()
         };
 
-        let message = `These Service Provider balances are below threshold:\n`;
+        let message = `SP Balances:\n`;
 
-        if (tanda.float <= env.TANDA_FLOAT_THRESHOLD) message += `\t - Tanda: ${tanda.float}\n`;
-        if (websms <= env.WEBSMS_THRESHOLD) message += `\t - Websms: ${websms}\n`;
-        if (AT.sms <= env.AT_SMS_THRESHOLD) message += `\t - AT SMS: ${AT.sms}\n`;
-        if (AT.airtime <= env.AT_AIRTIME_THRESHOLD) message += `\t - AT Airtime: ${AT.airtime}\n`;
+        if (tanda.float <= env.TANDA_FLOAT_THRESHOLD) message += `\t - Tanda Float: ${tanda.float}\n`;
         if (AT.ussd <= env.AT_USSD_THRESHOLD) message += `\t - AT USSD: ${AT.ussd}\n`;
+        // if (AT.sms <= env.AT_SMS_THRESHOLD) message += `\t - AT SMS: ${AT.sms}\n`;
+        // if (AT.airtime <= env.AT_AIRTIME_THRESHOLD) message += `\t - AT Airtime: ${AT.airtime}\n`;
+        if (websms <= env.WEBSMS_THRESHOLD) message += `\t - WebSMS: ${websms}\n`;
 
         if (message.includes('-')) {
             await NotificationRepository.store(
