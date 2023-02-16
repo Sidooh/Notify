577da0361e7252c29a80ad5615bf7fae7b5f7d4b
Nabcellent <nabcellent.dev@gmail.com>
2023-02-16 21:52:38 +0300

    Decouples system balance checks
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/jobs/ProcessSystemBalances.ts
+++ src/jobs/ProcessSystemBalances.ts
@@ -1,32 +1,28 @@
-import { schedule } from 'node-cron';
 import { log } from '../utils/logger';
-import { CONFIG } from '../config';
 import ATService, { ATApp } from '../channels/sms/AT/AT.service';
 import { Help } from '../utils/helpers';
 import NotificationRepository from '../repositories/notification.repository';
 import { Channel, EventType } from '../utils/enums';
 import { env } from '../utils/validate.env';
 
-export const ProcessSystemBalances = () => {
+export const ProcessSystemBalances = async () => {
     if (!env.SP_BALANCE_NOTIFICATION_ENABLED) return;
 
     log.info('...[JOB]... Setting up system balances job...');
 
-    schedule(CONFIG.sidooh.cron.sp_balance_notification_cron, async () => {
-        const smsSettings = await Help.getSMSSettings();
+    const smsSettings = await Help.getSMSSettings();
 
-        const AT = {
-            ussd: await new ATService(smsSettings.africastalking_env, ATApp.USSD).balance()
-        };
+    const AT = {
+        ussd: await new ATService(smsSettings.africastalking_env, ATApp.USSD).balance()
+    };
 
-        let message = `! System Balances Below Threshold:\n`;
+    let message = `! System Balances Below Threshold:\n`;
 
-        if (AT.ussd <= env.AT_USSD_THRESHOLD) message += `\t - AT USSD: ${AT.ussd}\n`;
+    if (AT.ussd <= env.AT_USSD_THRESHOLD) message += `\t - AT USSD: ${AT.ussd}\n`;
 
-        if (message.includes('-')) {
-            await NotificationRepository.store(
-                Channel.SMS, message, EventType.STATUS_UPDATE, env.ADMIN_CONTACTS.split(',')
-            );
-        }
-    });
+    if (message.includes('-')) {
+        await NotificationRepository.store(
+            Channel.SMS, message, EventType.STATUS_UPDATE, env.ADMIN_CONTACTS.split(',')
+        );
+    }
 };
