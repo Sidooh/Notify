40ed7d38146d493ead02de11c776aaa0c4e865b6
Nabcellent <nabcellent.dev@gmail.com>
2023-02-19 23:12:46 +0300

    Decouples system balance checks
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/http/controllers/job.controller.ts
+++ src/http/controllers/job.controller.ts
@@ -37,10 +37,7 @@ export class JobController extends Controller {
         const ATUSSDIsBelowThresh = africasTalking.ussd <= env.AT_USSD_THRESHOLD;
 
         if (wavesmsIsBelowThresh || websmsIsBelowThresh || ATAirtimeIsBelowBelowThresh || ATUSSDIsBelowThresh) {
-            message += `\t - WaveSMS: ${wavesms}\n
-                        \t - WebSMS: ${websms}\n
-                        \t - AT SMS: ${africasTalking.sms}\n
-                        \t - AT USSD: ${africasTalking.ussd}\n`;
+            message += `\t - WaveSMS: ${wavesms}\n\t - WebSMS: ${websms}\n\t - AT SMS: ${africasTalking.sms}\n\n\t - AT USSD: ${africasTalking.ussd}\n`;
         }
 
         if (message.includes('-')) {
