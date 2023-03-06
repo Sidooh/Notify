526baef0ecd4afc5e46cbaf048098b0093553d38
Nabcellent <nabcellent.dev@gmail.com>
2023-03-06 13:11:21 +0300

    Adds tests
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/repositories/__tests__/notification.repository.test.ts
+++ src/repositories/__tests__/notification.repository.test.ts
@@ -2,19 +2,20 @@ import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { Channel, EventType, Status } from '../../utils/enums';
 import db from '../../db/__mocks__/prisma';
 import NotificationRepository from '../notification.repository';
+import { NotFoundError } from '../../exceptions/not-found.err';
 
 const repo = new NotificationRepository;
 
 const notification = {
     id         : 1n,
-    channel: Channel.SMS,
-    content: 'test',
-    status: Status.COMPLETED,
-    event_type: EventType.TEST,
+    channel    : Channel.SMS,
+    content    : 'test',
+    status     : Status.COMPLETED,
+    event_type : EventType.TEST,
     destination: '254110039317',
-    created_at: new Date,
-    updated_at: new Date
-}
+    created_at : new Date,
+    updated_at : new Date
+};
 
 describe('notification.repository', () => {
     beforeEach(() => {
@@ -25,12 +26,39 @@ describe('notification.repository', () => {
         it('should return a list of notifications', async () => {
             db.notification.findMany.mockResolvedValueOnce([notification]);
 
-            const notifications = await repo.index({});
+            const result = await repo.index({});
 
-            expect(notifications).toStrictEqual([notification]);
+            expect(result).toStrictEqual([notification]);
         });
+    });
 
-        /*it('should create a notification if request data is valid.', async function() {
+    describe('find', () => {
+        it('should throw an error if not exists', () => {
+            db.notification.findUnique.mockResolvedValueOnce(null);
+
+            expect(() => repo.find(2)).rejects.toThrowError(NotFoundError);
+        });
+        it('should return a notification if exists', async function() {
+            db.notification.findUnique.mockResolvedValueOnce(notification);
+
+            const result = await repo.find(1);
+
+            expect(result).toStrictEqual(notification);
+        });
+    });
+
+    describe('update', () => {
+        it('should update a notification', async () => {
+            db.notification.update.mockResolvedValueOnce({ ...notification, status: Status.FAILED });
+
+            const result = await repo.update(notification, { id: notification.id });
+
+            expect(result).toStrictEqual({ ...notification, status: Status.FAILED });
+        });
+    });
+
+    /*describe('notify', () => {
+        it('should create a notification if request data is valid.', async function() {
             let data = {
                 channel    : Channel.SMS,
                 destination: [254110039317, 254736388405],
@@ -44,6 +72,6 @@ describe('notification.repository', () => {
 
             expect(notifications[0].channel).toEqual(data.channel);
             expect(notifications[0].event_type).toEqual(data.event_type);
-        });*/
-    });
+        });
+    })*/
 });
