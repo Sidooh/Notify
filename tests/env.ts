80ea5e32db33f832c20c909c8636ab16d09bce50
Nabcellent <nabcellent.dev@gmail.com>
2023-03-10 11:21:05 +0300

    Updates docker build
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- tests/env.ts
+++ tests/env.ts
@@ -1,5 +1,3 @@
-process.env.JWT_KEY = 'something';
-
 process.env.AT_SMS_API_KEY = 'something';
 process.env.AT_SMS_DEV_API_KEY = 'something';
 process.env.AT_SMS_DEV_USERNAME = 'something';
@@ -10,6 +8,11 @@ process.env.AT_USSD_API_KEY = 'something';
 process.env.AT_USSD_USERNAME = 'something';
 
 process.env.DATABASE_URL = 'something';
+
+process.env.JWT_KEY = 'something';
+
+process.env.MAIL_PASSWORD = 'something';
+process.env.MAIL_USERNAME = 'something';
 
 process.env.WAVE_SMS_API_KEY = 'something'
 process.env.WAVE_SMS_PARTNER_ID = 'something'
@@ -22,6 +25,3 @@ process.env.WEBSMS_DEV_API_KEY = 'something';
 process.env.WEBSMS_DEV_CLIENT_ID = 'something';
 process.env.WEBSMS_SENDER_ID = 'something';
 process.env.WEBSMS_DEV_SENDER_ID = 'something';
-
-process.env.MAIL_PASSWORD = 'something';
-process.env.MAIL_USERNAME = 'something';
