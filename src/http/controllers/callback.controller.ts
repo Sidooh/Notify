2bb93c7ff1611c453da40a8e44b33426b1e328d7
Nabcellent <nabcellent.dev@gmail.com>
2023-04-02 17:48:36 +0300

    Implements notification callbacks
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/http/controllers/callback.controller.ts
+++ src/http/controllers/callback.controller.ts
@@ -9,6 +9,7 @@ export class CallbackController extends Controller {
 
         this.router.all(`${this.basePath}/wavesms`, this.#wavesms);
         this.router.all(`${this.basePath}/websms`, this.#websms);
+        this.router.all(`${this.basePath}/africastalking`, this.#africastalking);
     }
 
     #wavesms = ({ body }: Request, res: Response) => {
@@ -26,6 +27,16 @@ export class CallbackController extends Controller {
             this.repo.handleCompleted(body.message_id)
         } else {
             this.repo.handleFailed(body.message_id)
+        }
+
+        res.send()
+    }
+
+    #africastalking = ({ body }: Request, res: Response) => {
+        if(body.status == 'Success') {
+            this.repo.handleCompleted(body.id)
+        } else {
+            this.repo.handleFailed(body.id)
         }
 
         res.send()
