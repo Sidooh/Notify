ebb569539703e705274ff20caf4eb14b3225237a
Nabcellent <nabcellent.dev@gmail.com>
2023-05-03 21:12:10 +0300

    Updates websms
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

---
+++ prisma/migrations/20230429222611_playdooh/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE `notifiables` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `notification_id` BIGINT UNSIGNED NOT NULL,
+    `provider` VARCHAR(255) NULL,
+    `message_id` VARCHAR(255) NULL,
+    `phone` VARCHAR(15) NULL,
+    `description` VARCHAR(255) NULL,
+    `status` VARCHAR(255) NOT NULL DEFAULT 'PENDING',
+    `cost` DECIMAL(8, 4) NOT NULL DEFAULT 0.0000,
+    `status_code` INTEGER NULL,
+    `updated_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `created_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    INDEX `FK_d844719248179dfd3336b3fc6d8`(`notification_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `notifications` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `channel` VARCHAR(20) NOT NULL,
+    `destination` VARCHAR(50) NOT NULL,
+    `event_type` VARCHAR(50) NOT NULL DEFAULT 'DEFAULT',
+    `content` TEXT NOT NULL,
+    `status` VARCHAR(255) NOT NULL DEFAULT 'PENDING',
+    `created_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `settings` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `key` VARCHAR(255) NOT NULL,
+    `value` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    UNIQUE INDEX `settings_key_key`(`key`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sms_providers` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `priority` TINYINT NOT NULL,
+    `environment` VARCHAR(20) NOT NULL,
+    `status` VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
+    `created_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` TIMESTAMP(6) NULL DEFAULT CURRENT_TIMESTAMP(6),
+
+    UNIQUE INDEX `sms_providers_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `notifiables` ADD CONSTRAINT `FK_d844719248179dfd3336b3fc6d8` FOREIGN KEY (`notification_id`) REFERENCES `notifications`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
