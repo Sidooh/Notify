8ee56ed95a178ca83fbb96be2135ce9d9d64f94e
Nabcellent <nabcellent.dev@gmail.com>
2023-06-24 10:22:24 +0300

    Fix for WaveSMS notifications that fail due to insufficient credits.
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
 const db = globalForPrisma.prisma || new PrismaClient({ log: ['error'] });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
